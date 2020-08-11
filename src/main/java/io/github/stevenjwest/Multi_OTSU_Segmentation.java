b2893029dc0cbc49326ebf8673cf37fbef267a55
stevenjwest <stevenjonwest@gmail.com>
2020-08-11 13:10:23 +0100

    Update Multi_OTSU_Segmentation.java and correct pom error with contributors

--- src/main/java/io/github/stevenjwest/Multi_OTSU_Segmentation.java
+++ src/main/java/io/github/stevenjwest/Multi_OTSU_Segmentation.java
@@ -1,52 +1,93 @@
-//
-// Multi_OtsuThreshold_.java
-//
-// Algorithm: PS.Liao, TS.Chen, and PC. Chung,
-//            Journal of Information Science and Engineering, vol 17, 713-727 (2001)
-// 
-// Coding   : Yasunari Tosa (ytosa@att.net)
-// Date     : Feb. 19th, 2005
-//
+/**
+ * Multi_OtsuThreshold_Stack.java
+ * 
+ * Algorithm: PS.Liao, TS.Chen, and PC. Chung,
+ * Journal of Information Science and Engineering, vol 17, 713-727 (2001)
+ * 
+ * v1:
+ * 
+ * Coding   : Yasunari Tosa (ytosa@att.net)
+ * Date     : Feb. 19th, 2005
+ * Site:	: https://github.com/cytomine/Cytomine-core/blob/master/src/java/be/cytomine/processing/image/filters/Multi_OtsuThreshold.java
+ * 
+ * v2:
+ * 
+ * MODIFIED	: Steven J. West (stevenjonwest@gmail.com)
+ * Date		: August 2019
+ * Site		: [PUT GITHUB REPO HERE]
+ * 
+ * The code has been modified to provide SUPPORT FOR STACK PROCESSING in StereoMate, and to directly modify
+ * the input image.  The output no longer generates a new output image or draws the histogram, and no longer shows each
+ * region in a new image (although it does still log the segmentation points between populations).
+ * 
+ */
+
+package io.github.stevenjwest;
+
 import ij.*;
 import ij.process.*;
 import ij.gui.*;
 import java.awt.*;
 import ij.plugin.filter.*;
 
-public class Multi_OtsuThreshold implements PlugInFilter 
-{
-  ImagePlus imp;
-  static final int NGRAY=256;
+public class Multi_OTSU_Segmentation implements PlugInFilter {
 
-  public int setup(String arg, ImagePlus imp) 
-  {
-    this.imp = imp;
-    return DOES_8G + NO_CHANGES;
-  }
+	ImagePlus imp;
+  
+	static final int NGRAY=256;
 
-  public void run(ImageProcessor ip) 
-  {
-    int MLEVEL = 4; // 3 level
+ 
+	
+	public int setup(String arg, ImagePlus imp) {
+		this.imp = imp;
+		return DOES_8G + NO_UNDO;
+	}
+
+	
+	
+  public void run(ImageProcessor ip) {
+    int MLEVEL = 3; // 3 level
+    int bkgroundLevel = 0;
+    boolean stack = true;
 
     GenericDialog gd = new GenericDialog("Select numLevels");
     String [] items= { "2", "3", "4", "5", };
     gd.addChoice("numLevels", items, "4");
+    String [] itemsBkground= { "0", "1", "2", "3", };
+    gd.addChoice("Background Selection", itemsBkground, "0");
+    gd.addCheckbox("stack", true);
     gd.showDialog();
     if (gd.wasCanceled())
       return;
 
     MLEVEL = gd.getNextChoiceIndex() + 2;
-    IJ.log("numLevels set to " + MLEVEL);
+    bkgroundLevel = gd.getNextChoiceIndex();
+    stack = gd.getNextBoolean();
+    //IJ.log("numLevels set to " + MLEVEL);
 
     int [] threshold = new int[MLEVEL]; // threshold
     int width = ip.getWidth();
     int height = ip.getHeight();
+    
+    ImageStack impStack = imp.getStack();
+    
+    //IJ.showMessage("Stack: "+impStack);
+    //IJ.showMessage("impStack size: "+impStack.getSize() );
+    
     ////////////////////////////////////////////
-    // Build Histogram
+    // Build Histogram - stack
     ////////////////////////////////////////////
-    float [] h = new float[NGRAY];
-    byte[] pixels = (byte[]) ip.getPixels();
-    buildHistogram(h, pixels, width, height);
+    float [] histogram = new float[NGRAY];
+    
+    byte[] pixels = null;
+    
+    for(int a=1; a<=impStack.getSize(); a++) {
+    	//byte[] pixels = (byte[]) ip.getPixels();
+    	pixels = (byte[]) impStack.getProcessor(a).getPixels();
+    	buildHistogram(histogram, pixels, width, height);
+    }
+    
+    computeProbabilityHistogram(histogram, width, height, impStack.getSize() );
 
     /////////////////////////////////////////////
     // Build lookup tables from h
@@ -54,7 +95,7 @@ public class Multi_OtsuThreshold implements PlugInFilter
     float [][] P = new float[NGRAY][NGRAY];
     float [][] S = new float[NGRAY][NGRAY];
     float [][] H = new float[NGRAY][NGRAY];
-    buildLookupTables(P, S, H, h);
+    buildLookupTables(P, S, H, histogram);
 
     ////////////////////////////////////////////////////////
     // now M level loop   MLEVEL dependent term
@@ -65,36 +106,76 @@ public class Multi_OtsuThreshold implements PlugInFilter
       msg += i + "=" + threshold[i] + ", ";
     msg += " maxSig = " + maxSig;
     IJ.log(msg);
+    IJ.log("histogram 0: "+histogram[0] + " histogram 10: "+histogram[10]);
 
     ///////////////////////////////////////////////////////////////
     // show regions works for any MLEVEL
     ///////////////////////////////////////////////////////////////
-    showRegions(MLEVEL, threshold, pixels, width, height);
+    showRegions(impStack, MLEVEL, bkgroundLevel, threshold, width, height);
   }
 
-  public void buildHistogram(float [] h, byte [] pixels, int width, int height)
-  {
+  
+  
+  /**
+   * Build the histogram from the byte array 'pixels', scanning over the image 'width' and 'height'.
+   * This method builds a histogram plot window.  It does not include BLACK [0] or WHITE [255] pixels.
+   * @param histogram
+   * @param pixels
+   * @param width
+   * @param height
+   */
+  public void buildHistogram(float [] histogram, byte [] pixels, int width, int height) {
     // note Java byte is signed. in order to make it 0 to 255 you have to
     // do int pix = 0xff & pixels[i];
-    for (int i=0; i < width*height; ++i)
-      h[(int) (pixels[i]&0xff)]++;
-   // note the probability of grey i is h[i]/(width*height)
-    float [] bin = new float[NGRAY];
-    float hmax = 0.f;
-    for (int i=0; i < NGRAY; ++i)
-    {
-      bin[i] = (float) i;
-      h[i] /= ((float) (width*height));
-      if (hmax < h[i])
-	hmax = h[i];
+    for (int i=0; i < width*height; ++i) {
+    	if( ((int) (pixels[i]&0xff) != 0) && ((int) (pixels[i]&0xff) != 255) ) {
+      histogram[(int) (pixels[i]&0xff)]++;
+    	}
     }
-    PlotWindow histogram = new PlotWindow("Histogram", "grey", "hist", bin, h);
-    histogram.setLimits(0.f, (float) NGRAY, 0.f, hmax);
-    histogram.draw();
+    // can ELIMINATE black [0] and white [255] pixels by logically testing the value of:
+    	// (int)(pixels[i]&0xff) -> if this is 0 or 255 the pixel is black or white
+    	// I think it makes sense for this algorithm to IGNORE these pixels!
+    		// AVOID OVER AND UNDER EXPOSURE ISSUES
+    
+    // This needs to act on the WHOLE HISTOGRAM once completely built of every Z slice
+    	// Moved to new method -> computeProbabilityHistogram()
+   // note the probability of grey i is h[i]/(width*height)
+    //float [] bin = new float[NGRAY];
+    //float hmax = 0.f;
+    //for (int i=0; i < NGRAY; ++i)
+    //{
+      //bin[i] = (float) i;
+      //histogram[i] /= ((float) (width*height));
+      //if (hmax < histogram[i])
+	//hmax = histogram[i];
+    //}
+    //PlotWindow histogramPlot = new PlotWindow("Histogram", "grey", "hist", bin, histogram);
+    //histogramPlot.setLimits(0.f, (float) NGRAY, 0.f, hmax);
+    //histogramPlot.draw();
+  }
+  
+  public void computeProbabilityHistogram(float[] histogram, int width, int height, int slices) {
+	  
+	   // note the probability of grey i is h[i]/(width*height)
+	// note the probability of grey i is h[i]/(width*height*slices) ??
+	    float [] bin = new float[NGRAY];
+	    float hmax = 0.f;
+	    for (int i=0; i < NGRAY; ++i) {
+	      bin[i] = (float) i;
+	      histogram[i] /= ((float) (width*height*slices));
+	      if (hmax < histogram[i]) {
+	    	  hmax = histogram[i];
+	      }
+	    }
+	    //PlotWindow histogramPlot = new PlotWindow("Histogram", "grey", "hist", bin, histogram);
+	    //histogramPlot.setLimits(0.f, (float) NGRAY, 0.f, hmax);
+	    //histogramPlot.draw();
+	  
   }
 
-  public void buildLookupTables(float [][] P, float [][] S, float [][] H, float [] h)
-  {
+  
+  
+  public void buildLookupTables(float [][] P, float [][] S, float [][] H, float [] h) {
     // initialize
     for (int j=0; j < NGRAY; j++)
       for (int i=0; i < NGRAY; ++i)
@@ -134,8 +215,9 @@ public class Multi_OtsuThreshold implements PlugInFilter
 
   }
 
-  public float findMaxSigma(int mlevel, float [][] H, int [] t)
-  {
+  
+  
+  public float findMaxSigma(int mlevel, float [][] H, int [] t) {
     t[0] = 0;
     float maxSig= 0.f;
     switch(mlevel)
@@ -200,33 +282,96 @@ public class Multi_OtsuThreshold implements PlugInFilter
     return maxSig; 
   }
 
-  public void showRegions(int mlevel, int [] t, byte [] pixels, int width, int height)
-  {
-    ImagePlus [] region = new ImagePlus[mlevel];
-    ImageProcessor [] rip = new ImageProcessor[mlevel];
-    for (int i=0; i < mlevel; ++i)
-    {
-      region[i] = NewImage.createByteImage("Region "+i, width, height,1, NewImage.FILL_BLACK);
-      rip[i] = region[i].getProcessor();
-    }
-    for (int i = 0; i < width*height; ++i)
-    {
-      int val = 0xff & pixels[i];
-      for (int k = 0; k < mlevel; k++)
-      {
-	if (k < mlevel-1)
-	{
-	  if (val < t[k+1] && val > t[k]) // k-1 region
-	    rip[k].putPixel(i%width, i/width, val);
-	}
-	else // k= mlevel-1 last region
-	{
-	  if (val > t[k])
-	    rip[k].putPixel(i%width, i/width, val);
-	}
-      }
-    }
-    for (int i=0; i < mlevel; i++)
-      region[i].show();
+  
+  
+  public void showRegions(ImageStack impStack, int mlevel, int bkgroundLevel, int [] threshold, int width, int height) {
+    //ImagePlus [] region = new ImagePlus[mlevel]; // do not use new imps - set the ips to the current imp!
+    // ImageProcessor rip = new ByteProcessor(width, height);
+    //for (int i=0; i < mlevel; ++i)
+    //{
+      // region[i] = NewImage.createByteImage("Region "+i, width, height,1, NewImage.FILL_BLACK);
+      // rip[i] = region[i].getProcessor();
+    	// rip[i] = new ByteProcessor(width, height);
+    //}
+	
+	  ImageProcessor rip = null;
+	  
+	  byte [] pixels = null;
+	  
+	  for(int a=1; a<=impStack.getSize(); a++) {
+
+		  rip = impStack.getProcessor(a);
+		  
+		  pixels = (byte[]) impStack.getProcessor(a).getPixels();
+
+		  for (int i = 0; i < width*height; ++i)
+		  {
+			  int pixelValue = 0xff & pixels[i];
+			  for (int k = 0; k < mlevel; k++)
+			  {
+				  if (k <= bkgroundLevel) 
+				  {
+					  if (pixelValue > 0 ) // k-0 region - BACKGROUND
+						  rip.putPixel(i%width, i/width, 0);
+				  }
+				  else if (k < mlevel-1)
+				  {
+					  if (pixelValue <= threshold[k+1] && pixelValue > threshold[k]) // k-1 region
+						  // rip[k].putPixel(i%width, i/width, pixelValue);
+						  rip.putPixel(i%width, i/width, 255); // set pixel value to max val - 255
+				  }
+				  else // k= mlevel-1 last region
+				  {
+					  if (pixelValue > threshold[k])
+						  // rip[k].putPixel(i%width, i/width, pixelValue);
+						  rip.putPixel(i%width, i/width, 255); // set pixel value to max val - 255
+				  }
+			  }
+		  }
+
+	  }
+    //for (int i=0; i < mlevel; i++)
+      //region[i].show();
+    
+    //imp.setProcessor(rip);
+    
+    imp.updateAndDraw();
+
   }
+  
+  
+  public void showRegions2(int mlevel, int [] threshold, byte [] pixels, int width, int height) {
+	    //ImagePlus [] region = new ImagePlus[mlevel]; // do not use new imps - set the ips to the current imp!
+	    ImageProcessor [] rip = new ImageProcessor[mlevel];
+	    for (int i=0; i < mlevel; ++i)
+	    {
+	      // region[i] = NewImage.createByteImage("Region "+i, width, height,1, NewImage.FILL_BLACK);
+	      // rip[i] = region[i].getProcessor();
+	    	rip[i] = new ByteProcessor(width, height);
+	    }
+	    for (int i = 0; i < width*height; ++i)
+	    {
+	      int pixelValue = 0xff & pixels[i];
+	      for (int k = 0; k < mlevel; k++)
+	      {
+		if (k < mlevel-1)
+		{
+		  if (pixelValue <= threshold[k+1] && pixelValue > threshold[k]) // k-1 region
+		    // rip[k].putPixel(i%width, i/width, pixelValue);
+		  	rip[k].putPixel(i%width, i/width, 255); // set pixel value to max val - 255
+		}
+		else // k= mlevel-1 last region
+		{
+		  if (pixelValue > threshold[k])
+		    // rip[k].putPixel(i%width, i/width, pixelValue);
+		  	rip[k].putPixel(i%width, i/width, 255); // set pixel value to max val - 255
+		}
+	      }
+	    }
+	    //for (int i=0; i < mlevel; i++)
+	      //region[i].show();
+
+	  }
+  
+  
 }
